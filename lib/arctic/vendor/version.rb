34ced1ff5b15ebcf4ca9a440546a952dc7a0f812
Rostyslav Semanyshyn <tetiross@gmail.com>
2019-11-28 14:28:53 +0200

    update gem version

--- lib/arctic/vendor/version.rb
+++ lib/arctic/vendor/version.rb
@@ -1,5 +1,5 @@
 module Arctic
   module Vendor
-    VERSION = '2.2.12'
+    VERSION = '2.3'
   end
 end
--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,5 +1,5 @@
 require "spec_helper"
 
 RSpec.describe 'VERSION' do
-  it { expect(Arctic::Vendor::VERSION).to eql '2.2.12' }
+  it { expect(Arctic::Vendor::VERSION).to eql '2.3' }
 end
