7ffd26e63f9eb733342358233333e76bc8d9a4fb
Rostyslav Semanyshyn <tetiross@gmail.com>
2020-10-15 17:55:41 +0300

    cleanup collection api

--- lib/arctic/vendor/collection_api.rb
+++ lib/arctic/vendor/collection_api.rb
@@ -4,29 +4,6 @@ module Arctic
   module Vendor
     module Collection
       class API < Arctic::Vendor::API
-        Error = Class.new StandardError
-        InvalidResponse = Class.new Error
-
-        # Collect orders from the Core API and updates them on the marketplace
-        def collect_orders(shop_id)
-          url = "shops/#{shop_id}/orders"
-
-          options = {
-            params: {
-              since: last_synced_at(shop_id, :orders),
-            },
-          }
-
-          orders = []
-
-          paginated_request(:get, url, options) do |response|
-            yield response.body if block_given?
-            orders.concat response.body
-          end
-
-          orders
-        end
-
         def order_dispersed( shop_id, order_id)
           request :patch, "shops/#{shop_id}/orders/#{order_id}/dispersed", body: {
             shop_id:shop_id,
